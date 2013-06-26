543e9158c12e8fa24c97453dcdd52163195beb3e
Ben Marshall <ben.marshall@stratfor.com>
2013-06-26 13:05:02 -0500

    Continuing work for the initial commit.

--- jquery.capture.js
+++ jquery.capture.js
@@ -22,12 +22,19 @@
   var vars = {
       isRecording: false,
       isPlaying: false,
-      events: []
+      events: [],
+      diff: [],
+      startTime: new Date(),
+      timeouts: [],
+      eventCounter: 0
     },
     config = {
       track: {
-        keyup: true
-      }
+        mousemove: true
+      },
+      mouseTimeout: 1000,
+      mouseFade: true,
+      mouseFadeDuration: 1000
     },
     elements = {
       toolbar: {
@@ -69,53 +76,97 @@
               break;
           }
         });
+        methods.updateToolbar();
         elements.toolbar.container.html(elements.toolbar.btnContainer);
         $('body').append(elements.toolbar.container);
       },
       updateToolbar: function() {
+        methods.updateVars();
         if (vars.isRecording) {
           elements.toolbar.btns.record.addClass('is-recording').removeClass('not-recording');
+          elements.toolbar.btns.play.removeClass('is-playing').addClass('not-playing disabled');
         } else {
           elements.toolbar.btns.record.addClass('not-recording').removeClass('is-recording');
+          elements.toolbar.btns.play.removeClass('disabled');
         }
         
         if (vars.isPlaying) {
-          elements.toolbar.btns.play.addClass('is-playing').removeClass('not-playing');
+          elements.toolbar.btns.play.addClass('is-playing icon-stop').removeClass('not-playing icon-play');
+          elements.toolbar.btns.record.removeClass('is-recording').addClass('not-recording disabled');
         } else {
-          elements.toolbar.btns.play.addClass('not-playing').removeClass('is-playing');
+          elements.toolbar.btns.play.addClass('not-playing icon-play').removeClass('is-playing icon-stop');
+          elements.toolbar.btns.record.removeClass('disabled');
         }
-        methods.updateVars();
       },
       updateVars: function() {
-        if (vars.isRecording) {
+        if (elements.toolbar.btns.record.hasClass('is-recording')) {
+          vars.isRecording = true;
           vars.isPlaying = false;
-        } else if (vars.isPlaying) {
+        }
+        if (elements.toolbar.btns.play.hasClass('is-playing')) {
           vars.isRecording = false;
+          vars.isPlaying = true;
         }
       },
       record: function(btn) {
         btn.toggleClass('not-recording is-recording');
         if (btn.hasClass('not-recording')) {
-          vars.isRecording = false;
           methods.events('unbind');
+          vars.isRecording = false;
         } else if(btn.hasClass('is-recording')) {
-          vars.isRecording = true;
           methods.events('bind');
+          vars.isRecording = true;
+          if (!vars.events.length) {
+            vars.startTime = new Date();
+          }
         }
       },
       play: function(btn) {
         btn.toggleClass('not-playing is-playing');
         if (btn.hasClass('not-playing')) {
           vars.isPlaying = false;
+          for (var i = 0; i < vars.timeouts.length; i++) {
+            window.clearTimeout(vars.timeouts[i]);
+          }
+          vars.timeouts = [];
+          methods.clearScreen();
         } else if(btn.hasClass('is-playing')) {
           vars.isPlaying = true;
           $.each(vars.events, function(i, o) {
             methods.playEvent(o);
           });
         }
+        /*setInterval(function() {
+          console.log(vars.timeouts);
+        }, 1000);*/
+      },
+      clearScreen: function() {
+        $('.capture-mouse').remove();
       },
       playEvent: function(e) {
-        console.log(e);
+        var id,
+          diff = e.timeStamp - vars.startTime.getTime();
+        switch(e.type) {
+          case 'mousemove':
+            id = 'capture-mouse-' + diff;
+            vars.timeouts.push(setTimeout(function() {
+              $('body').append($('<div />').addClass('capture-mouse').attr('id', id).css({
+                top: e.pageY,
+                left: e.pageX
+              }));
+              vars.timeouts.push(setTimeout(function() {
+                if (config.mouseFade) {
+                  $('#' + id).fadeOut(config.mouseFadeDuration, function() {
+                    $(this).remove();
+                  });
+                } else {
+                  $('#' + id).remove();
+                }
+              }, config.mouseTimeout));
+              
+            }, diff));
+            break;
+        }
       },
       events: function(a) {
         if (a === 'bind') {
@@ -145,7 +196,10 @@
 jQuery(function() {
   jQuery.capture({
     track: {
-      keyup: true
-    }
+      mousemove: true
+    },
+    mouseTimeout: 1000,
+    mouseFade: true,
+    mouseFadeDuration: 1000
   });
 });
